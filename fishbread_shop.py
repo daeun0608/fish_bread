a7214e91a69f87dbddb7d8a5cb498f35b9037358
daeun0608 <glace0608@naver.com>
2025-04-22 18:50:49 +0900

    ✨ 붕어빵 주문 기능 구현
    맛 오입력 검출, 주문받은 붕어빵 판매 기능, 판매된 붕어빵 개수 저장하는 기능

--- fishbread_shop.py
+++ fishbread_shop.py
@@ -1,15 +1,44 @@
 #주문, 관리자, 종료
+stock={
+    "팥붕어빵":10,
+    "슈크림붕아빵":8,
+    "초코붕어빵":5,
+}
+
+sales={
+    "팥붕어빵":0,
+    "슈크림붕아빵":0,
+    "초코붕어빵":0,
+}
+
+def order_bread():
+    while True:
+        bread_type=input("주문할 붕어빵 선택 (팥붕어빵,슈크림붕아빵,초코붕어빵)\n만약 뒤로가길 원한다면 '뒤로가기'룰 압력하세요. --> ")
+        if(bread_type=='뒤로가기'):
+            break
+        if bread_type in stock:     #in으로 확인하면 key만 확인한다
+            bread_count=int(input(f'{bread_type} 몇 개를 구입할까요? --> '))
+            if stock[bread_type]>=bread_count:
+                stock[bread_type]-=bread_count
+                sales[bread_type]+=bread_count
+                print(f'{bread_type} {bread_count}개가 판매되었습니다.')
+            else:
+                print(f'미안해, 지금 주문가능한 빵의 개수는 {stock[bread_type]}개야.')
+        else:
+            print('잘못된 입력입니다, 주문을 다시 해주세요.')
+def admin_mode():
+    pass
 
 while True:
     mode=input("원하는 모드를 선택하세요(주문, 관리자, 종료) : ")
-    
-    if mode=='주문' :
+
+    if mode=='종료' :
+        print("시스템이 종료되었습니다.")
+        break
+    elif mode=='주문' :
         order_bread()
     elif mode=='관리자' :
         admin_mode()
-    elif mode=='종료' :
-        print("시스템이 종료되었습니다.")
-        break
     else :
         print("잘못된 입력입니다. 다시 입력하세요.")
     
